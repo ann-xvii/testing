65a1036389ecabbaae286bc3c5282543010a0ad6
An.nA <anna.git3@gmail.com>
2015-01-22 15:44:21 -0800

    prep for model tests

---
+++ app/models/book.rb
@@ -0,0 +1,7 @@
+class Book < ActiveRecord::Base
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :pages, presence: true
+end
+
+
---
+++ spec/factories/books.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :book do
+    title "Generation P"
+	author "Victor Pelevin"
+	pages 300
+  end
+end
